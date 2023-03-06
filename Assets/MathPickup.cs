64c49158fb8be4d6ff9e177b429af7f78fd6e6bd
Xiaoli Tang <xiaolit@usc.edu>
2023-03-05 19:27:32 -0800

    math and display

--- Assets/MathPickup.cs
+++ Assets/MathPickup.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MathPickup : MonoBehaviour
@@ -14,5 +17,39 @@ public class MathPickup : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent.CompareTag("Player"))
+        {
+            string myText = gameObject.transform.Find("3D Text Prefab").GetComponent<TextScript>().EnterTextHere;
+            int num;
+            if (int.TryParse(myText, out num))
+            {
+                MathManager.instance.doMath(num);
+            }
+            else
+            {
+                switch (myText)
+                {
+                    case "+":
+                        MathManager.instance.currOperation = MathManager.operation.add;
+                        break;
+                    case "-":
+                        MathManager.instance.currOperation = MathManager.operation.subtract;
+                        break;
+                    case "*":
+                        MathManager.instance.currOperation = MathManager.operation.multiply;
+                        break;
+                    case "/":
+                        MathManager.instance.currOperation = MathManager.operation.divide;
+                        break;
+                    default:
+                        Debug.Log("invalid operator!");
+                        break;
+                }
+            }
+        }
     }
 }
