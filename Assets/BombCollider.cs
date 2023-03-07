c5db72e144ae1d2bf2b975677119ae1ad014328b
HARSHIL\Harshil <hrr.ind99@gmail.com>
2023-03-06 22:07:35 -0800

    TIMEBOMB1

--- Assets/BombCollider.cs
+++ Assets/BombCollider.cs
@@ -8,29 +8,36 @@ public class BombCollider : MonoBehaviour
     private float slowspeed;
     GameObject movingObstacleObject;
     int count = 0;
+    private bool isSlowing = false;
     void OnTriggerEnter(Collider other)
     {
         movingObstacleObject = GameObject.Find(other.gameObject.name);
         movingObstacle = movingObstacleObject.GetComponent<MovingObstacle>();
         // Debug.Log("SPEED:"+movingObstacle.speed);
-        if (movingObstacle != null)
+        if (movingObstacle != null && isSlowing == false) 
         {
+            isSlowing = true;
             if(count==0)
             {
                 count++;
                 slowspeed = movingObstacle.speed;
             }
-            movingObstacle.speed = 2.0f;
+        movingObstacle.speed = Mathf.Lerp(slowspeed, 2.0f , 5.0f);
+ 
         }
     }
 
+
     void OnDestroy()
     {
-        if (movingObstacle != null)
+        if (movingObstacle != null  && isSlowing == true)
         {
+            isSlowing = false;
             count = 0;
-            movingObstacle.speed = slowspeed;
-            Debug.Log("SLOW SPEED:"+slowspeed);
+            movingObstacle.speed = Mathf.Lerp(2.0f, slowspeed, 5.0f);
+            // Debug.Log("SLOW SPEED:"+slowspeed);
         }
     }
+
+
 }
